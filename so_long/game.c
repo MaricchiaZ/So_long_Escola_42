5371a0bf56fc4c2ca5512bdeaf62b06c13e19b09
maclara- <maclara-@student.42sp.org.br>
2022-12-20 15:25:05 +0100

    aviso se a xlaunch n for encontrado

--- so_long/game.c
+++ so_long/game.c
@@ -6,7 +6,7 @@
 /*   By: maclara- <maclara-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 14:09:49 by maclara-          #+#    #+#             */
-/*   Updated: 2022/12/19 16:18:35 by maclara-         ###   ########.fr       */
+/*   Updated: 2022/12/20 15:14:01 by maclara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,11 +101,17 @@ static int	game_draw(t_sl *game)
 
 void	game_work(t_sl *game)
 {
-	window(game);
+	int	check_wind;
+
+	check_wind = window(game);
+	if(check_wind)
+	{
 	open_images(game);
 	mlx_hook(game->pt_mlx.mlx_window, K_PRESS, 1L << 0, &recipe_key, game);
 	mlx_hook(game->pt_mlx.mlx_window, K_PRESS_X, 1L << 2, \
 	&destroyer_window, game);
 	mlx_loop_hook(game->pt_mlx.mlx, &game_draw, game);
-	mlx_loop(game->pt_mlx.mlx);
+	mlx_loop(game->pt_mlx.mlx);		
+	}
+	write(1, "Error\nmlx or Xlaunch not found\n", 31);
 }
